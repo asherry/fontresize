018ca1af48fb236f6b491540d142f73ac6ff0c2e
Alan <alan.sherry@freeflowdigital.com>
2015-10-27 19:26:49 -0700

    Added: new module for resizing fonts

---
+++ fontresize.js
@@ -0,0 +1,28 @@
+(function ($,Drupal) {
+  Drupal.behaviors.textResize = {
+    attach: function(context,settings) {
+      var inc = .5;
+      var toRem = function(px) {
+        var htmlPx = parseInt($('html').css('font-size'));
+        return Math.floor((parseInt(px) / htmlPx) * 100) / 100;
+      }
+      $('.fontresize-control').click(function(e) {
+        var $changer = $(this);
+        $('.resizable').each(function() {
+          $this = $(this);
+          var cur = $this.css('font-size');
+          if ($changer.attr('id') == 'text-resize-increase') {
+            $this.css('font-size', toRem(cur) + inc + 'rem');
+          }
+          else if ($changer.attr('id') == 'text-resize-decrease') {
+            $this.css('font-size', toRem(cur) - inc + 'rem');
+          }
+          else {
+            $this.css('font-size', 'inherit');
+          }
+        });
+        e.preventDefault();
+      });
+    }
+  }
+})(jQuery, Drupal);
